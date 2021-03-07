96c7671e105409e60f09bedc1ceac2d1a45fb4c2
maya <maya.keeley@outlook.com>
2021-03-07 00:51:00 +0000

    created grid with random mines

---
+++ main.js
@@ -0,0 +1,38 @@
+const emptyGrid = document.getElementById('grid');
+
+const rows = 10;
+const columns = 15;
+const mines = 40;
+
+const createMines = (mines, columns, rows) => {
+    const grid = new Array(rows).fill().map(row => {
+        return new Array(columns).fill(0);
+    });
+    const fields = columns * rows;
+    const numbers = Array(fields).fill().map((field, index) => index);
+    numbers.sort(() => (Math.random() - 0.5));
+    const rawMines = numbers.slice(0, mines);
+    rawMines.forEach(number => {
+        const x = Math.floor(number / columns);
+        const y = number % columns;
+        grid[x][y] = 'x';
+
+        for( let i = x - 1; i <= x + 1; i++) {
+            for (let j = y - 1; j <= y + 1; j++) {
+                // if co-ordinates are within bounds of grid
+                if (i >=0 && i < rows && j >=0 && j < columns) {
+                    // if co-ordinates are either side to side or up and down from mine
+                    if (Math.abs(i - x) + Math.abs(j - y) === 1) {
+                        // if co-ordinates do not contain a mine
+                        if (grid[i][j] !== 'x') {
+                            grid[i][j] = grid[i][j] + 1;
+                        }
+                    }
+                }
+            }
+        }
+    })
+    return grid;
+}
+const minefield = createMines(mines, columns, rows);
+console.log(minefield);
